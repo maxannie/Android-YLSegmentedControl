cb395ec13372dc956783f141eff82d02eafbc3a6
Yin Li <yin@sgs-lab.com>
2015-03-22 22:26:54 +0000

    Initial commit

---
+++ ylsegmentedbutton/src/main/java/com/yinli/ylsegmentedbutton/YLSegmentedRadioButton.java
@@ -0,0 +1,7 @@
+package com.yinli.ylsegmentedbutton;
+
+/**
+ * Created by yinli on 22/03/15.
+ */
+public class YLSegmentedRadioButton {
+}
---
+++ ylsegmentedbutton/src/main/java/com/yinli/ylsegmentedbutton/YLSegmentedRadioGroup.java
@@ -0,0 +1,25 @@
+package com.yinli.ylsegmentedbutton;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioGroup;
+
+/**
+ * Created by yinli on 22/03/15.
+ */
+public class YLSegmentedRadioGroup extends RadioGroup {
+
+    public YLSegmentedRadioGroup(Context context) {
+        this(context, null);
+    }
+
+    public YLSegmentedRadioGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private void init() {
+
+    }
+
+    
+}
